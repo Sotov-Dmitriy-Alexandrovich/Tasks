cfbbff05a223530551dcb8702b1ce76a88307222
Dmitriy Sotov <sotov@dmitriy-alexandrovich.ru>
2026-06-19 19:11:08 +0500

    Сделал CRUD для users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,20 @@ class UserController extends Controller
         $user = User::create($request->all());
         return $user;
     }
+    public function index(){
+        return User::all();
+    }
+    public function destroy($id){
+        User::find($id)->delete();
+        return response()->json(
+            "User с id $id Удален"
+        );
+    }
+    public function update(Request $request, $id){
+        $user = User::find($id);
+        User::find($id)->update($request->all());
+        return response()->json(
+            "Пользователь с id $id обновлен"
+        );
+    }
 }
