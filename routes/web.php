3141c0c4e92b7d4cd69bbaf1c03d2fe84f317d1b
Dmitriy Sotov <sotov@dmitriy-alexandrovich.ru>
2026-06-21 03:25:59 +0500

    Создал Web-интерфейс для register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/register',function() {
     return view('auth.register');
-});
+})->name('register.form');
+Route::post('/register', [UserController::class, 'store'])->name('register');
